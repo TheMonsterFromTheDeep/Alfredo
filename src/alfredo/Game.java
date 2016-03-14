0d4b81e432fdbf69a244d0f39204de40ff26551c
TheMonsterFromTheDeep <themonsterfromthedeep@gmail.com>
2016-03-14 16:16:46 -0600

    Make Game have constant window proportion; minimum size
    The game will always be rendered consistently now as the Game draws a
    buffer with the game's graphical data to fit the screen.
    
    The Game window itself now has a minimum size.

--- src/alfredo/Game.java
+++ src/alfredo/Game.java
@@ -5,7 +5,10 @@ import alfredo.input.Mouse;
 import alfredo.scene.Scene;
 import alfredo.timing.Interval;
 import java.awt.Graphics;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.awt.event.MouseAdapter;
+import java.awt.image.BufferedImage;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -15,7 +18,16 @@ public class Game extends Interval {
     private static final int DEFAULT_WIDTH = 600;
     private static final int DEFAULT_HEIGHT = 400;
     
-    private class GamePanel extends JPanel {       
+    private static final int MIN_WIDTH = 128;
+    private static final int MIN_HEIGHT = 64;
+    
+    private final class GamePanel extends JPanel {
+        BufferedImage buffer;
+        Graphics bufGraphics;
+        
+        int bufWidth, bufHeight; //The size that the buffer is drawn at
+        int bufX, bufY; //The position that the buffer is drawn at
+        
         public GamePanel(int width, int height) {
             this.setSize(width, height);
             this.setPreferredSize(new java.awt.Dimension(width, height));
@@ -26,23 +38,59 @@ public class Game extends Interval {
             addMouseListener(adapter);
             addMouseWheelListener(adapter);
             addMouseMotionListener(adapter);
+            
+            buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+            bufGraphics = buffer.createGraphics();
         }
         
         @Override
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
             scene.draw();
+            g.drawImage(buffer, bufX, bufY, bufWidth, bufHeight, null);
             repaint();
+        }
+        
+        private void doGraphics() {
+            //Recalculates size and offset of graphical buffer
+            if((float)panel.getWidth() / buffer.getWidth() > (float)panel.getHeight() / buffer.getHeight()) {
+                bufHeight = panel.getHeight();
+                bufWidth = (int)(bufHeight * ((float)buffer.getWidth() / buffer.getHeight()));
+                bufX = (panel.getWidth() - bufWidth) / 2;
+                bufY = 0;
+            }
+            else {
+                bufWidth = panel.getWidth();
+                bufHeight = (int)(bufWidth * ((float)buffer.getHeight() / buffer.getWidth()));
+                bufY = (panel.getHeight() - bufHeight) / 2;
+                bufX = 0;
+            }
         }
     }
     
-    private class GameFrame extends JFrame {
+    private final class GameFrame extends JFrame {
         public GameFrame(String title) {
             super(title);
             
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             
             addKeyListener(Keys.init());
+            
+            this.setMinimumSize(new java.awt.Dimension(MIN_WIDTH, MIN_HEIGHT));
+            this.addComponentListener(new ComponentAdapter() {
+                @Override
+                public void componentResized(ComponentEvent e) {
+                    int width = GameFrame.this.getWidth();
+                    int height = GameFrame.this.getHeight();
+                    
+                    if (width < MIN_WIDTH) { width = MIN_WIDTH; }
+                    if (height < MIN_HEIGHT) { height = MIN_HEIGHT; }
+                    
+                    GameFrame.this.setSize(width, height);
+                    
+                    panel.doGraphics();
+                }
+            });
         }
     }
     
