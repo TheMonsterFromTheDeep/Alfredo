b2f95f809305b04882a7a2caa9d0f2e8481ae271
TheMonsterFromTheDeep <themonsterfromthedeep@gmail.com>
2016-03-14 16:57:25 -0600

    Make Resources catch all exceptions
    This may be bad practice, but it will prevent weirdness with exceptions
    that aren't ioexceptions; generally exceptions thrown by image loading
    shouldn't simply kill a program

--- src/alfredo/util/Resources.java
+++ src/alfredo/util/Resources.java
@@ -3,7 +3,6 @@ package alfredo.util;
 import alfredo.paint.Graphic;
 import alfredo.paint.ImageGraphic;
 import alfredo.paint.NullGraphic;
-import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class Resources {
@@ -11,7 +10,7 @@ public class Resources {
         try {
             return new ImageGraphic(ImageIO.read(Resources.class.getResource(path)));
         }
-        catch (IOException e) {
+        catch (Exception e) {
             System.err.println("Failed to load image resource (" + path + "): " + e.getLocalizedMessage());
             return new NullGraphic();
         }
