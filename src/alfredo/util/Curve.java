1836b3759a2abf0eb026c622d2032b2dddcde641
TheMonsterFromTheDeep <themonsterfromthedeep@gmail.com>
2017-04-15 23:02:16 -0600

    Change Curve to use cubic splines
    These are soooo much better. Things will actually go through all the
    control points, making these several magnitudes more useful.

--- src/alfredo/util/Curve.java
+++ src/alfredo/util/Curve.java
@@ -30,27 +30,104 @@ import alfredo.geom.Vector;
  * @author TheMonsterOfTheDeep
  */
 public class Curve {
-    public Vector[] controlPoints;
+    private static class Segment {
+        Vector a, b, c, d;
+        
+        public Segment(Vector[] V, Vector[] D, int index) {
+            if(index == V.length - 1) {
+                throw new IllegalArgumentException("For N points, there should only be N - 1 segments!");
+            }
+            
+            a = V[index];
+            b = D[index];
+            //Why Java don't you have operator overloading
+            c = V[index + 1].minus(V[index]).times(3).minus(D[index].times(2)).minus(D[index + 1]);
+            d = V[index].minus(V[index + 1]).times(2).plus(D[index]).plus(D[index + 1]);
+        }
+        
+        public Vector evaluate(float t) {
+            return a.plus(b.times(t)).plus(c.times(t * t)).plus(d.times(t * t *t));
+        }
+    }
+    
+    public final Vector[] controlPoints;
+    private final Segment[] segments;
     
     public Curve(Vector... controlPoints) {
         this.controlPoints = controlPoints;
+        segments = new Segment[controlPoints.length - 1];
+        
+        calculate();
     }
     
-    public static int binomialCoefficient(int n, int k) {
-        float cof = 1;
-        for(int i = 1; i <= k; ++i) {
-            cof *= (n + 1 - i) / (float)i;
+    private double getk(float[] in, int index) {
+        if(index == 0) {
+            return 3 * (in[1] - in[0]);
         }
-        return (int)Math.round(cof);
+        if(index == in.length - 1) {
+            return 3 * (in[in.length - 1] - in[in.length - 2]);
+        }
+        else {
+            return 3 * (in[index + 1] - in[index - 1]);
+        }
+    }
+    
+    private double solveCof(float[] in, float[] out, double clast, double mlast, int index) {
+        double m, c;
+        if(index == out.length - 1) {
+            m = 2 - (1 / mlast);
+            c = (getk(in, index) - clast) / m;
+            out[index] = (float)c;
+            return c;
+        }
+        else if(index == 0) {
+            m = 2;
+            c = getk(in, 0) / m;
+        }
+        else {
+            m = 4 - (1 / mlast);
+            c = (getk(in, index) - clast) / m;
+        }
+        double next = solveCof(in, out, c, m, index + 1);
+        double result = c - (next / m);
+        out[index] = (float)result;
+        return result;
+    }
+    
+    private void calculate() {
+        Vector[] solved = new Vector[controlPoints.length];
+        for(int i = 0; i < solved.length; ++i) {
+            solved[i] = new Vector();
+        }
+        
+        float[] in = new float[controlPoints.length];
+        float[] out = new float[controlPoints.length];
+        for(int i = 0; i < in.length; ++i) {
+            in[i] = controlPoints[i].x;
+        }
+        solveCof(in, out, 0, 0, 0);
+        
+        for(int i = 0; i < in.length; ++i) {
+            solved[i].x = out[i];
+            in[i] = controlPoints[i].y;
+        }
+        solveCof(in, out, 0, 0, 0);
+        
+        for(int i = 0; i < out.length; ++i) {
+            solved[i].y = out[i];
+        }
+        
+        for(int i = 0; i < segments.length; ++i) {
+            segments[i] = new Segment(controlPoints, solved, i);
+        }
     }
     
     public Vector evaluate(float timestep) {
-        Vector point = new Vector();
-        for(int i = 0; i < controlPoints.length; ++i) {
-            int com = binomialCoefficient(controlPoints.length - 1, i);
-            point.x += com * Math.pow((1 - timestep), controlPoints.length - i - 1) * Math.pow(timestep, i) * controlPoints[i].x;
-            point.y += com * Math.pow((1 - timestep), controlPoints.length - i - 1) * Math.pow(timestep, i) * controlPoints[i].y;
-        }
-        return point;
+        calculate();
+        
+        int bucket = (int)(timestep * segments.length);
+        if(bucket > segments.length - 1) { bucket = segments.length - 1; }
+        
+        return segments[bucket].evaluate(timestep * segments.length - bucket);
     }
 }
